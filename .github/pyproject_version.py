0063fb1b5527ad03b61b210d71eb63fd1f3194fc
Mahdi Ben Jelloul <mahdi.benjelloul@gmail.com>
2026-04-09 16:02:18 +0100

    Fix

--- .github/pyproject_version.py
+++ .github/pyproject_version.py
@@ -2,17 +2,22 @@
 
 import argparse
 import re
+from pathlib import Path
+
+
+class VersionNotFoundError(Exception):
+    """Raised when the package version is not found in pyproject.toml."""
 
 
 def get_version():
     """Read package version from pyproject.toml."""
-    with open("./pyproject.toml") as file:
+    with Path("./pyproject.toml").open() as file:
         content = file.read()
     version_match = re.search(r'^version\s*=\s*"([\d.]*)"', content, re.MULTILINE)
     if version_match:
         return version_match.group(1)
     msg = "Package version not found in pyproject.toml"
-    raise Exception(msg)
+    raise VersionNotFoundError(msg)
 
 
 if __name__ == "__main__":
